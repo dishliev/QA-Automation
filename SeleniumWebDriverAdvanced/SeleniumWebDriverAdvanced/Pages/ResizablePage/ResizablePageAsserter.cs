81aeac160824d14651d0a2e18bf70993e37085e7
Kiril Dishliev <kiril.dishliev1@gmail.com>
2017-05-08 13:28:27 +0300

    Update ResizablePageAsserter.cs

--- SeleniumWebDriverAdvanced/SeleniumWebDriverAdvanced/Pages/ResizablePage/ResizablePageAsserter.cs
+++ SeleniumWebDriverAdvanced/SeleniumWebDriverAdvanced/Pages/ResizablePage/ResizablePageAsserter.cs
@@ -1,4 +1,4 @@
-﻿using NUnit.Framework;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +9,28 @@ namespace SeleniumWebDriverAdvanced.Pages.ResizablePage
 {
     public static class ResiblePageAsserter
     {
-        public static void AssertNewSize(this ResizablePage page, int pixelToWidth, int pixelsToheight)
+        public static void AssertNewSize(this ResizablePage page)
         {
-            Assert.AreEqual(page.Width + 100, page.resizeWindow.Size.Width);
-            Assert.AreEqual(page.Height + 100, page.resizeWindow.Size.Height);
+
+            Assert.IsTrue(page.resizeWindow.Size.Width > 332 && page.resizeWindow.Size.Width < 335);
+            Assert.IsTrue(page.resizeWindow.Size.Height > 332 && page.resizeWindow.Size.Height < 335);
+       
+        }
+        public static void AssertNewSizeConstrain(this ResizablePage page)
+        {
+
+            Assert.IsTrue(page.resizableConstrainWindow.Size.Width < 183 && page.resizableConstrainWindow.Size.Width > 178);
+            Assert.IsTrue(page.resizableConstrainWindow.Size.Height > 91 && page.resizableConstrainWindow.Size.Height < 95);
+
+          
+        }
+        public static void AssertNewSizeHelperBox(this ResizablePage page)
+        {
+       
+            Assert.IsTrue(page.HelperBox.Size.Width > 290 && page.HelperBox.Size.Width < 300);
+            Assert.IsTrue(page.HelperBox.Size.Height > 290 && page.HelperBox.Size.Height < 300);
+
+
         }
     }
 }
