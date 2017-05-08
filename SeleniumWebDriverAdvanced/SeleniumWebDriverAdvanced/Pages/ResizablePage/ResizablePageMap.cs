b5d72177c9ce0b97f7984b8ad2516a36c69748c3
Kiril Dishliev <kiril.dishliev1@gmail.com>
2017-05-08 13:28:02 +0300

    Update ResizablePageMap.cs

--- SeleniumWebDriverAdvanced/SeleniumWebDriverAdvanced/Pages/ResizablePage/ResizablePageMap.cs
+++ SeleniumWebDriverAdvanced/SeleniumWebDriverAdvanced/Pages/ResizablePage/ResizablePageMap.cs
@@ -1,4 +1,4 @@
-﻿using OpenQA.Selenium;
+using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,5 +24,47 @@ namespace SeleniumWebDriverAdvanced.Pages.ResizablePage
                 return this.Driver.FindElement(By.Id("resizable"));
             }
         }
+        public IWebElement resizeButtonConstrainResizeArea
+        {
+            get
+            {
+                return this.Driver.FindElement(By.XPath("//*[@id=\"resizableconstrain\"]/div[3]"));
+            }
+        }
+        public IWebElement ConstrainResizeArea
+        {
+            get
+            {
+                return this.Driver.FindElement(By.XPath("//*[@id=\"ui-id-3\"]"));
+            }
+        }
+          public IWebElement resizableConstrainWindow
+        {
+            get
+            {
+                return this.Driver.FindElement(By.Id("resizableconstrain"));
+            }
+        }
+          public IWebElement HelperBox
+          {
+              get
+              {
+                  return this.Driver.FindElement(By.XPath("//*[@id=\"resizable_helper\"]"));
+              }
+          }
+         public IWebElement Helper
+          {
+              get
+              {
+                  return this.Driver.FindElement(By.XPath("//*[@id=\"ui-id-4\"]"));
+              }
+          }
+         public IWebElement resizableButtonHelperBox
+         {
+             get
+             {
+                 return this.Driver.FindElement(By.XPath("//*[@id=\"resizable_helper\"]/div[3]"));
+             }
+         }
     }
 }
